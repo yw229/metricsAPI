f46e1e98c1ad41c95b77fbdf05380f12cd4894df
Elsa Wang <ewang@simulmedia.com>
2017-02-26 10:53:20 -0500

    add cases

--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,7 @@ var fs = require("fs");
 var path = require("path");
 var _ = require("underscore");
 
-
 var jsonPath = path.join(__dirname, '..', 'models','metrics.json');
-
 var ctrl = require('../controller/metricController.js');
 
 var appRouter = function(app) {
@@ -25,24 +23,34 @@ var appRouter = function(app) {
             var existed = _.where(json,{"timestamp": time}); //check if timestamp existed 
 
             //Scenario1 : Add a measurement with valid (numeric) values and timestamp
-            if ( existed.length ===0 && time && ctrl.isValidTimestamp(time) && ctrl.isNumeric(temp) && ctrl.isNumeric(dp) && ctrl.isNumeric(precp)) {
-            	var newm = {};
-            		newm.timestamp = time;
-            		newm.temperature = parseFloat(temp); 
-            		newm.dewPoint = parseFloat(dp); 
-            		newm.precipitation = parseFloat(precp);
-            	json.push(newm);
-            	console.log(json);
+            if (existed.length ===0){
+                if ( time && ctrl.isValidTimestamp(time) && ctrl.isNumeric(temp) && ctrl.isNumeric(dp) && ctrl.isNumeric(precp)) {
+                    var newm = {};
+                        newm.timestamp = time;
+                        newm.temperature = parseFloat(temp); 
+                        newm.dewPoint = parseFloat(dp); 
+                        newm.precipitation = parseFloat(precp);
+                    json.push(newm);
+                    console.log(json);
 
-            	fs.writeFile(jsonPath, JSON.stringify(json), function(err) {
-    				if (err) throw err;
-    				console.log('The "data to append" was appended to file!');
-                    res.status(201).send({ "status": "201", "location header ": "/measurements/" + time });
-				});
-            } 
-            else // Scenario 2 and 3: Cannot add a measurement with invalid values or without a timestamp
+                    fs.writeFile(jsonPath, JSON.stringify(json), function(err) {
+                        if (err) throw err;
+                        console.log('The "data to append" was appended to file!');
+                        res.status(201).send({ "status": "201", "location header ": "/measurements/" + time });
+                    });
+                }
+                else  // Scenario 2: Cannot add a measurement with invalid values
+                    if( !ctrl.isNumeric(temp) || ! ctrl.isNumeric(dp) || ! ctrl.isNumeric(precp) ){
+                        res.status(400).send({ 'status': '400','error':'invalid values'});
+                    }
+                else //Scenario 3: Cannot add a measurement without a timestamp
+                    if(!time || ctrl.isValidTimestamp(time)){ 
+                        res.status(400).send({ 'status': '400','error':'missing or invalid timestamp '});
+                    }
+            }
+            else // 
             {
-                res.status(400).send({ 'status': '400' });
+                res.status(400).send({ 'status': '400','error':time+' already existed'});
             }
         })
 
@@ -52,12 +60,15 @@ var appRouter = function(app) {
 
     // check route params, if full timestamp, go to /:timestamp, else go /:date
 	app.param('timestamp', function(req, res, next, value, name) {
-		console.log('param',value,name);
-    	if (!ctrl.isDayOnly(value)) {
+		console.log('param',value);
+    	if (ctrl.isValidTimestamp(value)) {
         	next();
-    	} else {
+    	} else if(ctrl.isDayOnly(value)){
         	next('route');
     	}
+        else{
+            res.status(400).send({ 'status': '400','error':'invalid date in request parameter '});
+        }
 	}) ;
 
 
@@ -76,7 +87,7 @@ var appRouter = function(app) {
             	res.json(rt[0]);
             }
             else if(rt.length === 0 ){ //Scenario 5 : Get a measurement that does not exist
-            	res.status(400).send({'status':'400'});
+            	res.status(400).send({'status':'400','error':'measurement does not exist'});
             }
         });
     });
@@ -111,31 +122,49 @@ var appRouter = function(app) {
             
             var param = req.params.timestamp;
             var time = req.body.timestamp,
-    			temp = req.body.temperature,
+                temp = req.body.temperature,
     			dp = req.body.dewPoint,
     			precp = req.body.precipitation;
-         	var metrics = JSON.parse(data);
-            var updated ={};
-         	console.log('put',param);
-            //Scenario8: Replace a measurement with valid (numeric) values
-            if(ctrl.isValidTimestamp(time) && ctrl.isNumeric(temp) && ctrl.isNumeric(dp) && ctrl.isNumeric(precp)){
-                _.each(metrics, function(obj){
-                        _.each(obj, function(v,k){
-                        if( v === param){
-                            console.log(obj);
-                            obj.temperature = parseFloat(temp);
-                            obj.dewPoint = parseFloat(dp) ;
-                            obj.precipitation = parseFloat(precp);
-                            updated = obj ; 
-                        }
-                    })
-                });
-                fs.writeFile(jsonPath, JSON.stringify(metrics), function(err) {
-                        if (err) throw err;
-                        console.log('The "data to replace" was updated to file!');
-                        res.status(204).send({ "status": "204", "updated measurement": updated });
-                });
-            }       
+         	var metrics = JSON.parse(data),
+                existed = _.where(metrics,{"timestamp": param}), //check if timestamp existed 
+                updated ={};
+         	console.log('put',param,time,existed);
+            
+            //measurement exists 
+            if(existed.length===1){
+                //Scenario8: Replace a measurement with valid (numeric) values
+                if( param === time && ctrl.isNumeric(temp) && ctrl.isNumeric(dp) && ctrl.isNumeric(precp)){
+                    _.each(metrics, function(obj){
+                            _.each(obj, function(v,k){
+                            if( v === param){
+                                console.log(obj);
+                                obj.temperature = parseFloat(temp);
+                                obj.dewPoint = parseFloat(dp) ;
+                                obj.precipitation = parseFloat(precp);
+                                updated = obj ; 
+                            }
+                        })
+                    });
+                    fs.writeFile(jsonPath, JSON.stringify(metrics), function(err) {
+                            if (err) throw err;
+                            console.log('The "data to replace" was updated to file!');
+                            res.status(204).send({ "status": "204", "updated measurement": updated });
+                    });
+                }
+                else //Scenario9: Replace a measurement with invalid values
+                    if (param === time &&(!ctrl.isNumeric(temp) || ! ctrl.isNumeric(dp) || ! ctrl.isNumeric(precp))){ 
+                        res.status(400).send({ "status": "400","measurement": existed[0]});
+                } 
+                else //Scenario 10 : Replace a measurement with mismatched timestamps
+                    if ( param !== time){
+                        res.status(409).send({ "status": "409", "measurement": existed[0]});
+                    }     
+            }
+            else  // Scenario11: Replace a measurement that does not exist
+                if( existed.length===0){
+                    res.status(404).send({ "status": "404" ,"error":"measurement does not exist"});
+                }
+
         });
     });
 
